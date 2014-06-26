6d0f391e56e27c01db5044560f672eef5411aa62
Lou <lougrimaud@gmail.com>
2014-06-26 13:53:12 -0500

    conclusion on eni extraction

--- Figures/figure_reformat.m
+++ Figures/figure_reformat.m
@@ -11,19 +11,20 @@ for i=1:length(names)
     
 display off
     openfig(names(i).name)
+
+    set(findall(gcf,'Type','text'),'FontSize',12) % change labels and legend size
+    set(findall(gcf,'type','axes')(1:end),'fontsize',10) % change axis numbers size
+    set(findall(gcf,'type','line'),'LineWidth',2) % change line width
+    set(findall(gcf,'type','line'),'MarkerSize',9) % changes marker size
+     
+%     legend(findall(gcf,'type','axes'),'Location','Best')
+ 
      set(gcf,'WindowStyle','normal')
      set(gcf, 'PaperUnits', 'inches');
 set(gcf, 'PaperPosition', [0 0 6 4.2]);
-%     set(gcf,'units','inches','outerposition',[0 0 4.8 3.6])
-
-    set(findall(gcf,'Type','text'),'FontSize',12) % change labels and legend size
-    set(findall(gcf,'type','axes'),'fontsize',12) % change axis numbers size
-    set(findall(gcf,'type','line'),'LineWidth',2) % change line width
-    set(findall(gcf,'type','line'),'MarkerSize',9) % changes marker size
-%      set(findall(gcf,'type','legend'),'position','best') % changes marker size
-%     legend(findall(gcf,'type','axes'),'Location','Best')
-%  pause
-%     saveas(gcf,names(i).name(1:end-4),'fig')
+    set(gcf,'units','inches','outerposition',[0 0 6 4.2])
+set(findall(gcf,'type','legend'),'Position','best') % changes marker size
+    saveas(gcf,names(i).name(1:end-4),'fig')
 
     saveas(gcf,names(i).name(1:end-4),'epsc')
     
